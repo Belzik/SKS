d41c6128f8f83c6bd2342afdd9a5f160dbf1a6c0
Katrych Alexander <pazaz1@mail.ru>
2019-06-29 12:52:49 +0300

    Initial Commit

---
+++ SKS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SKS
+//
+//  Created by Александр Катрыч on 29/06/2019.
+//  Copyright © 2019 Katrych. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
