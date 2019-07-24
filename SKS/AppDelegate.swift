dd3d99d7e5519222c3795406e77e1b10e18f6e3f
Katrych Alexander <pazaz1@mail.ru>
2019-07-24 22:12:43 +0300

    [Mobile/SKS-82][task] Layout profile

--- SKS/AppDelegate.swift
+++ SKS/AppDelegate.swift
@@ -19,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         IQKeyboardManager.shared.enable = true
         IQKeyboardManager.shared.enableAutoToolbar = false
+        UIApplication.statusBarBackgroundColor = UIColor(hexString: "#383C45")
+        application.statusBarStyle = .lightContent
         return true
     }
 
